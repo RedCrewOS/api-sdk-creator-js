bfa753539a741833caae9b52fd11bcd9ed0a671d
Kieran Simpson <kieran@redcrew.com.au>
2020-11-10 15:46:48 +1100

    Update client to only use unstructured data types.

--- http-client/src/http-client.ts
+++ http-client/src/http-client.ts
@@ -1,11 +1,24 @@
 import { HttpRequest } from "./http-request";
 import { HttpResponse } from "./http-response";
 
+/**
+ * Data type that does not have a pre-defined data model/type definition.
+ */
+export type UnstructuredData = Buffer | NodeJS.ReadableStream | ReadableStream | string;
+
+/**
+ * A HttpClient deals strictly with unstructured data types. To use a HttpClient with structured
+ * data (JSON, XML, etc) consumers of the HttpClient will need to be able to marshal and
+ * unmarshal structured data to/from unstructured data types.
+ *
+ * @see {@link JsonHttpClient}
+ */
 export interface HttpClient {
 	/**
 	 * A request will always return a response regardless of what the response represents (eg: success or failure).
 	 *
 	 * Errors should only be used to indicate unrecoverable problems like a network being unavailable.
+	 *
 	 */
-	makeRequest(request: HttpRequest): Promise<HttpResponse>;
+	makeRequest(request: HttpRequest<UnstructuredData>): Promise<HttpResponse<UnstructuredData>>;
 }
