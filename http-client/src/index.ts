57a797511cff5a3bdc618429883387b32e0a4e25
Kieran Simpson <kieran@redcrew.com.au>
2021-01-28 12:25:18 +1100

    Update unmarshalling to handle all unstructured data types.

--- http-client/src/index.ts
+++ http-client/src/index.ts
@@ -3,4 +3,5 @@ export * from "./http-client";
 export * from "./http-request";
 export * from "./http-response";
 export * from "./json-marshaller";
+export * from "./obj-merge";
 export * from "./unstructured-data";
--- http-client/src/json-marshaller.ts
+++ http-client/src/json-marshaller.ts
@@ -1,29 +1,48 @@
 "use strict";
 
-import { HttpRequestPolicy, HttpResultHandler } from "./http-client";
+import { HttpRequestPolicy, HttpResult, HttpResultHandler } from "./http-client";
 import { HttpRequest } from "./http-request";
+import { UnstructuredData, unstructuredDataToString } from "./unstructured-data";
 
 const Async = require("crocks/Async");
 
+const chain = require("crocks/pointfree/chain");
+const getPath = require("crocks/Maybe/getPath");
 const hasProp = require("crocks/predicates/hasProp");
 const ifElse = require("crocks/logic/ifElse");
+const flip = require("crocks/combinators/flip");
+const map = require("crocks/pointfree/map");
 const mapProps = require("crocks/helpers/mapProps");
+const maybeToResult = require("crocks/Result/maybeToResult");
 const not = require("crocks/logic/not");
 const pipe = require("crocks/helpers/pipe");
+const pipeK = require("crocks/helpers/pipeK");
 const resultToAsync = require("crocks/Async/resultToAsync");
 const setPath = require("crocks/helpers/setPath");
 const tryCatch = require("crocks/Result/tryCatch");
+
+import objMerge from "./objMerge";
 
 // very loose definition what what's convertible to/from JSON
 export type JSONObject = Record<string, any>;
 
 export const JSON_MIME_TYPE = "application/json";
 
-const responseMapping = {
-	response: {
-		body: (x: string) => JSON.parse(x)
-	}
-};
+// unstructuredDataBodyToStringBody :: HttpResult UnstructuredData -> Async HttpResult string
+const unstructuredDataBodyToStringBody = (result: HttpResult): typeof Async => {
+	const path = [ "response", "body" ];
+
+	return pipe(
+		resultToAsync(maybeToResult(new Error("Missing response body"), getPath(path))),
+		chain(unstructuredDataToString),
+		map(flip(setPath(path))({})),
+		map(objMerge(result)),
+	)(result);
+}
+
+// parseResponseBodyToJson :: HttpResult string -> Async HttpResult JSONObject
+const parseResponseBodyToJson: (result: HttpResult<any, string>) => typeof Async =
+	resultToAsync(tryCatch(mapProps({ response: { body: JSON.parse } })))
 
 export function jsonMarshaller(
 	contentType: string = JSON_MIME_TYPE
@@ -33,17 +52,16 @@ export function jsonMarshaller(
 		Async.of,
 		pipe(
 			setPath([ "headers", "content-type" ], contentType),
-			tryCatch((request: HttpRequest) =>
+			resultToAsync(tryCatch((request: HttpRequest) =>
 				setPath([ "body" ], JSON.stringify(request.body), request)
-			),
-			resultToAsync,
+			))
 		),
 	) as HttpRequestPolicy<JSONObject>;
 }
 
-export function jsonUnmarshaller(): HttpResultHandler<string, JSONObject> {
-	return pipe(
-		tryCatch(mapProps(responseMapping)),
-		resultToAsync
-	) as HttpResultHandler<string, JSONObject>;
+export function jsonUnmarshaller(): HttpResultHandler<UnstructuredData, JSONObject> {
+	return pipeK(
+		unstructuredDataBodyToStringBody,
+		parseResponseBodyToJson
+	) as HttpResultHandler<UnstructuredData, JSONObject>;
 }
