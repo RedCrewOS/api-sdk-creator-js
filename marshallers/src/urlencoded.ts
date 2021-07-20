53d7c8e6ce3c44f8055f8151daf7f24e35c2f4b6
Kieran Simpson <kieran@redcrew.com.au>
2021-07-20 13:22:34 +1000

    Add urlencoded submodule.

---
+++ marshallers/src/urlencoded.ts
@@ -0,0 +1,5 @@
+// encode :: Object -> String
+export const encode: (data: Record<any, any>) => string = () => ""
+
+// decode :: String -> Object
+export const decode: (data: string) => Record<any, any> = () => ({})
--- marshallers/test/webpack/Marshallers.spec.ts
+++ marshallers/test/webpack/Marshallers.spec.ts
@@ -4,4 +4,8 @@ describe("Marshallers global", function () {
 	it("should have Marshallers module", function() {
 		assertThat(Marshallers, is(defined()));
 	});
+
+	it("should have urlencoded submodule", function() {
+		assertThat(Marshallers.urlencoded, is(defined()));
+	});
 });
