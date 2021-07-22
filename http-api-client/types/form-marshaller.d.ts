b7f02975196d987efb3641f99efe70d6052c01c4
Kieran Simpson <kieran@redcrew.com.au>
2021-07-22 21:22:58 +1000

    Fix doco.

--- http-api-client/types/form-marshaller.d.ts
+++ http-api-client/types/form-marshaller.d.ts
@@ -5,7 +5,7 @@ import { UnstructuredData } from "./unstructured-data";
 export const URL_ENCODED_MIME_TYPE: string;
 
 /**
- * @param {string} [contentType] Defaults to JSON_MIME_TYPE
+ * @param {string} [contentType] Defaults to URL_ENCODED_MIME_TYPE
  */
 export function formMarshaller(contentType?: string): HttpRequestPolicy<Record<string, string>>;
 
