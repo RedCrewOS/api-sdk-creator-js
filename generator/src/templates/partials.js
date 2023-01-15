9fa0a8ab12b323971017db6d8474d04c9e56ab6c
Kieran Simpson <kieran@redcrew.com.au>
2023-01-15 21:14:32 +1100

    Refactored to make code clearer

--- generator/src/templates/partials.js
+++ generator/src/templates/partials.js
@@ -3,18 +3,19 @@
 const Async = require("crocks/Async");
 
 const ap = require("crocks/pointfree/ap");
+const compose = require("crocks/helpers/compose");
 const flip = require("crocks/combinators/flip");
-const pipe = require("crocks/helpers/pipe");
 
 const { languageDir, readTemplatesFromDir } = require("./templates");
 const { registerPartial } = require("./wrappers");
 
+// applyToHandlebars :: Async Error (Handlebars -> Handlebars) -> Handlebars -> Async Error Handlebars
+const applyToHandlebars = (fn) =>
+	compose(flip(ap, fn), Async.of)
+
 // addPartials :: String -> Handlebars -> Async Error Handlebars
 const addPartials = (language) =>
-	pipe(
-		Async.of,
-		flip(ap, readTemplatesFromDir(registerPartial, languageDir(language)))
-	)
+	applyToHandlebars(readTemplatesFromDir(registerPartial, languageDir(language)))
 
 module.exports = {
 	addPartials
