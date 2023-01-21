10b895154d4036e755cc22be8015c71c955d459c
Kieran Simpson <kieran@redcrew.com.au>
2023-01-21 15:51:44 +1100

    Update index to run generator

--- generator/src/index.js
+++ generator/src/index.js
@@ -1,3 +1,30 @@
-module.exports = {
-	add: (a, b) => a + b
-}
+"use strict";
+
+const curry = require("crocks/helpers/curry");
+const pipeK = require("crocks/helpers/pipeK");
+const resultToAsync = require("crocks/Async/resultToAsync");
+
+const { getOpts } = require("./getopts");
+const { parseFile } = require("./parser");
+const { renderComponentsObject } = require("./visitors/renderer");
+
+(async function main() {
+	// parseOpts :: Async Error Object
+	const parseOpts = resultToAsync(getOpts)
+
+	// generator :: Object -> Async Error ComponentsObject
+	const generator = curry(({ language, outdir, spec }) =>
+		pipeK(
+			parseFile,
+			renderComponentsObject(outdir, language)
+		)(spec)
+	)
+
+	// program :: [ String ] -> Async Error ComponentsObject
+	const program = pipeK(
+		parseOpts,
+		generator
+	);
+
+	await program(process.argv).toPromise();
+}());
