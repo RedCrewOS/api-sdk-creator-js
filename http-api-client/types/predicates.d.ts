3326d2e37af3dd8b3b5faf7909786d6c4b03643f
Kieran Simpson <kieran@redcrew.com.au>
2023-02-13 15:59:44 +1100

    Update definition with exported predicates.

--- http-api-client/types/predicates.d.ts
+++ http-api-client/types/predicates.d.ts
@@ -1,6 +1,14 @@
 import { HttpResponse } from "./response";
 import { HttpResult } from "./client";
 
+export function hasContentType(contentType: string, headerValue: string): boolean;
+export function hasContentType(contentType: string): (headerValue: string) => boolean;
+
+export function isRelativeUrl(value: string): boolean;
+
 export function isSuccessfulResponse(response: HttpResponse): boolean;
 
 export function isSuccessfulResult(result: HttpResult): boolean;
+
+export function resultHasContentType(contentType: string, result: HttpResult): boolean;
+export function resultHasContentType(contentType: string): (result: HttpResult) => boolean;
